ed05b04f52521fccdb39ae5b00372bac12da8e66
Christopher Bloom <bloomcb@gmail.com>
2021-07-29 19:26:00 -0700

    Hacking react

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,81 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type Tab = 'appointments' | 'crews';
+
+type BranchSelectProps = {
+  branches: string[];
+  activeBranch: string;
+  changeBranch: (e: any) => void;
+};
+
+const BranchSelect = ({
+  branches,
+  activeBranch,
+  changeBranch,
+}: BranchSelectProps) => {
+  return (
+    <select value={activeBranch} onChange={changeBranch}>
+      {branches.length &&
+        branches.map((item) => (
+          <option value={item} key={item}>
+            {item}
+          </option>
+        ))}
+    </select>
+  );
+};
+
+const fetchBranches = ['Branch 1', 'Branch 2', 'Branch 3'];
+
 function App() {
+  const [activeTab, setActiveTab] = useState<Tab>('appointments');
+
+  const [branches, setBranches] = useState<string[]>([]);
+  const [activeBranch, setActiveBranch] = useState('');
+
+  useEffect(() => {
+    setTimeout(() => setBranches(fetchBranches), 5000);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <button onClick={() => setActiveTab('appointments')}>
+            Appointments
+          </button>
+          <button onClick={() => setActiveTab('crews')}>Crews</button>
+        </div>
+
+        <div className={`hidden ${activeTab === 'appointments' && 'visible'}`}>
+          <h1>CONTENT OF APPOINTMENTS TABS</h1>
+
+          {branches.length ? (
+            <BranchSelect
+              branches={branches}
+              activeBranch={activeBranch}
+              changeBranch={(e) => setActiveBranch(e.target.value)}
+            />
+          ) : (
+            'Loading'
+          )}
+
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+
+        <div className={`hidden ${activeTab === 'crews' && 'visible'}`}>
+          <h1>CONTENT OF CREWS TAB</h1>
+          <BranchSelect
+            branches={branches}
+            activeBranch={activeBranch}
+            changeBranch={(e) => {
+              console.log(e);
+              setActiveBranch(e.target.value);
+            }}
+          />
+        </div>
       </header>
     </div>
   );
